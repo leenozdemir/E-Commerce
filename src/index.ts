e412f10fadeda846242e6cb7e1cea081fd87eb3d
leenozdemir <145616931+leenozdemir@users.noreply.github.com>
2024-09-10 11:55:27 +0300

    Product Featuer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./router/userRoute";
+import { seedInitialProducts } from "./services/productService";
+import productRoute from "./router/productRoute";
 
 const app = express();
 const port = 3001;
+
+// Seed the products to database
+seedInitialProducts();
 
 app.use(express.json());
 
@@ -13,6 +18,7 @@ mongoose
   .catch((err) => console.log("Failed to connect!", err));
 
   app.use("/user", userRoute);
+  app.use("/product", productRoute);
 
 app.listen(port, () => {
   console.log(`Server is running at: http://localhost:${port}`);
